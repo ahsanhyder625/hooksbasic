9d5b1e4cf1e0f13d9a8dd2b5aae5c29e2ca1ef71
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-11-12 18:00:29 +0530

    Increment Decrement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './index.css'
 
 function App() {
+  const [count, setcount] = useState(0)
+
+  const handleIncrement = () =>{
+    setcount(count+1);
+  }
+
+  const handleDecrement = () =>{
+    setcount(count-1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{count}</h1>
+        <button onClick={handleIncrement}>Click me to Increment!</button>
+        <button onClick={handleDecrement}>Click me to Decrement!</button>
     </div>
   );
 }
